446ecd4b98694f78b96b30ceaad3b98ae06e23bd
Chris <fulgencc@uci.edu>
2019-08-04 18:30:31 -0700

    initial commit

---
+++ ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+public class ChatHub : Hub
+{
+    public void SendToAll(string name, string message)
+    {
+        Clients.All.SendAsync("sendtoall", name, message);
+    }
+
+    public void AddNewUser(string userName)
+    {
+        Clients.All.SendAsync("newuserconnected", userName);
+    }
+
+    public void UserDisconnected(string userName)
+    {
+        Clients.All.SendAsync("userdisconnected");
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception exception)
+    {
+        return base.OnDisconnectedAsync(exception);
+    }
+}
